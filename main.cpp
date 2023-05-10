d6ff19a3b68c5152882c2cad7bf9b36b7ceafb1f
Nikostreet11 <nico.pollini@gmail.com>
2023-05-10 14:43:07 +0900

    Remove fine grain parallelization

--- main.cpp
+++ main.cpp
@@ -184,107 +184,6 @@ int imageCompositionOmp(cv::Mat& foreground, std::vector<cv::Mat>& backgrounds, 
 	return 0;
 }
 
-int imageCompositionOmpFineGrain(cv::Mat& foreground, std::vector<cv::Mat>& backgrounds, int transformations)
-{
-	// random initialization
-	std::random_device rd;
-	std::mt19937 gen(rd());
-	std::uniform_int_distribution<int> uniform;
-
-	// compute output folder name
-	std::time_t t = std::time(nullptr);
-	std::tm* now = std::localtime(&t);
-	std::stringstream folderName;
-	folderName <<
-			   (now->tm_year + 1900) << '-' <<
-			   (now->tm_mon + 1) << '-' <<
-			   now->tm_mday << '-' <<
-			   now->tm_hour << '-' <<
-			   now->tm_min << '-' <<
-			   now->tm_sec;
-
-	// create output folder
-	std::stringstream outputFolderStream;
-	outputFolderStream << "../output/" << folderName.str();
-	std::string outputFolderPath = outputFolderStream.str();
-	fs::create_directories(outputFolderPath);
-
-	int row;
-	int col;
-	int alpha;
-	cv::Mat background;
-
-	#pragma omp parallel default(none) shared(foreground, backgrounds, background, uniform, gen, row, col, alpha) firstprivate(transformations, outputFolderPath)
-	{
-		int threadId = omp_get_thread_num();
-		//printf("Thread %d> Hello!\n", threadId);
-
-		for (int count = 0; count < transformations; ++count)
-		{
-			#pragma omp single
-			{
-				//printf("Thread %d> Starting iteration %d...\n", threadId, count);
-
-				// copy a random background from the shared pool
-				int index = (int) (uniform(gen) % backgrounds.size());
-				background = backgrounds.at(index).clone();
-
-				// check the background dimensions
-				if (background.rows < foreground.rows || background.cols < foreground.cols) {
-					printf("ERROR: the foreground exceeds the background dimensions");
-				}
-
-				// pick a random position for the foreground
-				if (background.rows - foreground.rows == 0) { row = 0; }
-				else { row = (int) (uniform(gen) % (background.rows - foreground.rows)); }
-				if (background.cols - foreground.cols == 0) { col = 0; }
-				else { col = (int) (uniform(gen) % (background.cols - foreground.cols)); }
-
-				// pick a random alpha value
-				alpha = 128 + uniform(gen) % 128;
-			}
-			float alphaCorrectionFactor = (float) alpha / 255;
-			float betaCorrectionFactor = 1 - alphaCorrectionFactor;
-
-			//printf("Thread %d> Arrived at for loop\n", threadId);
-			#pragma omp barrier
-			for (int i = 0; i < foreground.rows; ++i)
-			{
-				/*if (i == 0)
-				{
-					printf("Thread %d> Working on image...\n", threadId);
-				}*/
-				#pragma omp for
-				for (int j = 0; j < foreground.cols; ++j)
-				{
-					//printf("Thread %d> Working on pixel\t[%d,%d]...\n", threadId, i, j);
-					cv::Vec4b &f_pixel = foreground.at<cv::Vec4b>(i, j);
-					cv::Vec4b &b_pixel = background.at<cv::Vec4b>(row + i, col + j);
-					float f_alpha = (float) f_pixel[3] / 255;
-					if (f_alpha > 0.9)
-					{
-						b_pixel[0] = (unsigned char) ((float) b_pixel[0] * betaCorrectionFactor +
-													  (float) f_pixel[0] * alphaCorrectionFactor * f_alpha);
-						b_pixel[1] = (unsigned char) ((float) b_pixel[1] * betaCorrectionFactor +
-													  (float) f_pixel[1] * alphaCorrectionFactor * f_alpha);
-						b_pixel[2] = (unsigned char) ((float) b_pixel[2] * betaCorrectionFactor +
-													  (float) f_pixel[2] * alphaCorrectionFactor * f_alpha);
-					}
-				}
-			}
-
-			#pragma omp single
-			{
-				std::string outputPath = outputFolderPath + "/out_" + std::to_string(count) + ".png";
-				//printf("Thread %d> Saving at %s...\n", threadId, outputPath.c_str());
-				cv::imwrite(outputPath, background);
-				//printf("Thread %d> Iteration %d complete.\n", threadId, count);
-			}
-		}
-	}
-	return 0;
-}
-
 int main()
 {
 	std::string background_path = "../input/background";
@@ -316,7 +215,7 @@ int main()
 	}
 
 	auto start_time = high_resolution_clock::now();
-	imageCompositionOmp(foreground, backgrounds, TRANSFORMATIONS);
+	imageComposition(foreground, backgrounds, TRANSFORMATIONS);
 	auto end_time = high_resolution_clock::now();
 
 	float totalTime = (float) duration_cast<microseconds>(end_time - start_time).count() / 1000.f;
